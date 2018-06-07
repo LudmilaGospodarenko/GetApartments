9ba3618762fd1d4e5874761afc819121443d3103
LudmilaGospodarenko <cathadow@mail.ru>
2018-06-08 00:46:29 +0300

    Get info from Onliner and parse it into txt file

---
+++ GetApartments/GetApartments/JSONDeserialized.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onliner
+{
+    public class Location
+    {
+        public string address { get; set; }
+        public string user_address { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+
+        public override string ToString()
+        {
+            return address + "\r\n";
+        }
+    }
+
+    public class BYN
+    {
+        public string amount { get; set; }
+        public string currency { get; set; }
+
+        public override string ToString()
+        {
+            return $"{currency}: {amount}\r\n";
+        }
+    }
+
+    public class USD
+    {
+        public string amount { get; set; }
+        public string currency { get; set; }
+        public override string ToString()
+        {
+            return $"{currency}: {amount}\r\n";
+        }
+    }
+
+    public class Converted
+    {
+        public BYN BYN { get; set; }
+        public USD USD { get; set; }
+    }
+
+    public class Price
+    {
+        public string amount { get; set; }
+        public string currency { get; set; }
+        public Converted converted { get; set; }
+        public override string ToString()
+        {
+            return $"{currency}: {amount}\r\n";
+        }
+    }
+
+    public class Area
+    {
+        public double total { get; set; }
+        public double living { get; set; }
+        public double kitchen { get; set; }
+        public override string ToString()
+        {
+            return $"Общая площадь {total}, жилая площадь {living}, кухня {kitchen}\r\n";
+        }
+    }
+
+    public class Seller
+    {
+        public string type { get; set; }
+        public override string ToString()
+        {
+            return $"Продавец: {type}\r\n";
+        }
+    }
+
+    public class AuctionBid
+    {
+        public string amount { get; set; }
+        public string currency { get; set; }
+    }
+
+    public class Apartment
+    {
+        public int id { get; set; }
+        public int author_id { get; set; }
+        public Location location { get; set; }
+        public Price price { get; set; }
+        public bool resale { get; set; }
+        public int number_of_rooms { get; set; }
+        public int floor { get; set; }
+        public int number_of_floors { get; set; }
+        public Area area { get; set; }
+        public string photo { get; set; }
+        public Seller seller { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime last_time_up { get; set; }
+        public int up_available_in { get; set; }
+        public string url { get; set; }
+        public AuctionBid auction_bid { get; set; }
+        public override string ToString()
+        {
+            return $"Расположен: {location.ToString()}" +
+                $"Цена: {price.ToString()}" +
+                $"Перепродажа: {resale.ToString()}\r\n" +
+                $"Всего комнат: {number_of_rooms.ToString()}\r\n" +
+                $"Этаж: {floor.ToString()}\r\n" +
+                $"Всего этажей: {number_of_floors.ToString()}\r\n" +
+                $"Площадь: {area.ToString()}" +
+                $"{seller.ToString()}" +
+                $"Объявление подано: {created_at.ToString()}\r\n" +
+                $"Последний раз поднято: {last_time_up.ToString()}\r\n" +
+                $"Ссылка: {url}\r\n" +
+                $"\r\n";
+
+        }
+    }
+
+    public class Page
+    {
+        public int limit { get; set; }
+        public int items { get; set; }
+        public int current { get; set; }
+        public int last { get; set; }
+    }
+
+    public class RootObject
+    {
+        public List<Apartment> apartments { get; set; }
+        public int total { get; set; }
+        public Page page { get; set; }
+
+        public override string ToString()
+        {
+            var str = $"Квартиры, страница {page.current}: \r\n";
+            foreach (var a in apartments)
+                str += a.ToString();
+            return str;
+        }
+    }
+}
---
+++ GetApartments/GetApartments/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Onliner
+{
+    public class Initial
+    {
+        const string url =
+            "https://pk.api.onliner.by/search/apartments?price%5Bmin%5D=4696&price%5Bmax%5D=60000&currency=usd&bounds%5Blb%5D%5Blat%5D=53.70077162898384&bounds%5Blb%5D%5Blong%5D=27.36900329589844&bounds%5Brt%5D%5Blat%5D=54.09483886777795&bounds%5Brt%5D%5Blong%5D=27.754211425781254&page={0}&_=0.40143548690752895";
+
+        static void Main()
+        {
+            Task t = new Task(DownloadPageAsync);
+            t.Start();
+            Console.WriteLine("Downloading page...");
+            Console.ReadKey();
+        }
+
+        static async void DownloadPageAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                //client.DefaultRequestHeaders.Add("Content-Type", "application/json");
+                for (int page = 1; page <= 15; page++)
+                    using (HttpResponseMessage response = await client.GetAsync(string.Format(url, page)))
+                    using (HttpContent content = response.Content)
+                    {
+                        string result = await content.ReadAsStringAsync();
+                        var json = JsonConvert.DeserializeObject<RootObject>(result);
+                        if (json.page.current < page)
+                        {
+                            Console.WriteLine($"Last page ({json.page.current}) proceeded, check output files.");
+                            break;
+                        }
+
+                        using (StreamWriter w = File.AppendText(@"json.txt"))
+                        {
+                            w.WriteLine(result);
+                        }
+                        using (StreamWriter w = File.AppendText(@"result.txt"))
+                        {
+                            w.WriteLine(json.ToString());
+                        }
+                        Console.WriteLine($"page {page} proceeded.");
+                    }
+                Console.WriteLine("All pages proceeded, check output files.");
+            }
+        }
+    }
+
+
+}
